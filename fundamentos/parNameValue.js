3bf56678883c4ac14d0c4ea004a74dac80fc111b
CorSar <1cesar.ccorreia@gmail.com>
2020-12-14 12:17:53 +0000

    Web-Modern-Course

---
+++ fundamentos/parNameValue.js
@@ -0,0 +1,22 @@
+//par name/value
+const greetings = 'Sup' // lexical context 1
+
+function exec() {
+    const greetings = 'Wassup' //lexical context 2
+    return greetings
+}
+
+//Objects are nested groups of name/value
+const client = {
+    name : 'Peter',
+    age : '32',
+    weight : '89',
+    adress: {
+        place : 'United States, Florida',
+        number : '13'
+    }
+}
+
+console.log(greetings)
+console.log(exec())
+console.log(client)
