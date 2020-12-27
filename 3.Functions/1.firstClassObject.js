ceafecfd213aac0c92149a910bdd6d0dd562e176
CorSar <1cesar.ccorreia@gmail.com>
2020-12-27 21:06:16 +0000

    Many ways of creating a function
    In this file you will find many ways to create/build a function which can help you in the most varible situations

---
+++ 3.Functions/1.firstClassObject.js
@@ -0,0 +1,35 @@
+//Function in JS is First-Class Object(Citizens)
+//Higher-order function
+
+//Creating in a literal form
+function fun1(){ }
+
+//Storing in a variable
+const fun2 = function() { }
+
+//Storing in an array
+const array = [function (a,b){return a + b}, fun1, fun2]
+console.log(array[0](2,3))
+
+//Storing in an object attribution
+const obj = {}
+obj.speak = function() { return 'Wassup' }
+console.log(obj.speak())
+
+//Funcion as a parameter
+function run(fun) {
+    fun()
+}
+
+run(function () {console.log('Executing...')})
+
+// A function can return/contain another function
+function some(a, b) {
+    return function (c) {
+        console.log(a+b+c)
+    }
+}
+
+some(2, 3)(5)
+const plus = some(2,3)
+plus(4)
