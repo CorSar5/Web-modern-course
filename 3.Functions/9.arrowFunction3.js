266dfd48a8da0b2ec094ea8efb7874f1d68f5402
CorSar <1cesar.ccorreia@gmail.com>
2021-01-01 19:27:20 +0000

    3rd Lesson of Arrow Learning
    In this file/portion of code there are some experiments from Arrow and the usage of this

---
+++ 3.Functions/9.arrowFunction3.js
@@ -0,0 +1,18 @@
+let compairWithThis = function (param){
+    console.log(this === param)
+}
+
+compairWithThis(global)
+
+const obj = {}
+compairWithThis = compairWithThis.bind(obj)
+compairWithThis(global) // false 
+compairWithThis(obj)// true, because of the bind usage/function
+
+let compairWithThisArrow = param => console.log(this === param)
+compairWithThisArrow(global) // false, there's no variation of the "this" form
+compairWithThisArrow(module.exports) //Actually = to 'this'
+
+compairWithThisArrow = compairWithThisArrow.bind(obj)// The Arrow won't be changed
+compairWithThisArrow(obj) //False returning
+compairWithThisArrow(module.exports)
