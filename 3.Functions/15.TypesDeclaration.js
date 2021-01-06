d5aed9e6debe61d92902b726911896e474e19778
CorSar <1cesar.ccorreia@gmail.com>
2021-01-06 20:20:40 +0000

    Types of Declaration of a Function
    In this file there's an advertisement to the usage of the console.log() even before the script that includes the code, in which will that work and which won't

--- 3.Functions/15.TypesDeclaration.js
+++ 3.Functions/15.TypesDeclaration.js
@@ -10,8 +10,10 @@ function sum(x, y){
 const sub = function (x,y){
     return x - y
 }
+console.log(sub(3, 4))
 
 //named function expression
 const mult = function mult(x, y){
     return x*y
 }
+console.log(mult(3, 4))
