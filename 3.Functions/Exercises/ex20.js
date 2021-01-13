9bf8ef33dfa6353ea2237012b6370e50c834c56d
CorSar <1cesar.ccorreia@gmail.com>
2021-01-13 20:16:27 +0000

    Update ex20.js

--- 3.Functions/Exercises/ex20.js
+++ 3.Functions/Exercises/ex20.js
@@ -1,11 +1,11 @@
-function money(x){
+function money(x) {
     let c100 = 0
     let c50 = 0
-    let c10= 0
+    let c10 = 0
     let c5 = 0
     let c1 = 0
     let value = valor(x)
-    while (x >= value){
+    while (x >= value) {
         switch (value) {
             case 100:
                 x -= 100
@@ -24,12 +24,13 @@ function money(x){
                 c5++
                 break
             case 1:
-                c++
+                c1++
                 value -= 1
                 break
         }
+
         value = valor(x)
-        break
+    
     }
     return elab(c100,c50,c10,c5,c1)
 }
@@ -37,40 +38,36 @@ function money(x){
 function valor(x){
     if (x >= 100){
         return 100
-    }
-    else if (x >= 50){
+    }else if (x >= 50){
         return 50
-    }
-    else if (x >= 10){
+    }else if (x >= 10){
         return 10
-    }
-    else if (x >= 5){
+    }else if (x >= 5){
         return 5
-    }
-    else if (x >= 1){
+    }else if (x >= 1){
         return 1
     }
 }
 
-function elab(c100,c50,c10,c5,c1){
+function elab(c100,c50,c10,c5,c1) {
     let result = ''
 
     if(c100>0){
-        result += `${c100} notes of 100€. `
+        result += `${c100} note(s) of 100€. `
     }
     if(c50>0){
-        result += `${c50} notes of 50€.`
+        result += `${c50} note(s) of 50€.`
     }
     if(c10>0){
-        result += `${c10} notes of 10€.`
+        result += `${c10} note(s) of 10€.`
     }
     if(c5>0){
-        result += `${c5} notes of 5€.`
+        result += `${c5} note(s) of 5€.`
     }
     if(c1>0){
-        result += `${c1} notes of 1€.`
+        result += `${c1} note(s) of 1€.`
     }
     return result
 }
 
-console.log(money(153));
+console.log(money(123));
