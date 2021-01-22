f040a119e90bb441a78a9d5ad2ee2bd7e9834cd5
CorSar <snakexdlol@gmail.com>
2021-01-22 22:43:22 +0000

    Examples of Object Creation
    In this file there are plenty ways of creating a Object, hope it can help you!

---
+++ 04.Object/03-Creating_Objects.js
@@ -0,0 +1,44 @@
+//using literal notation
+const obj1 = {}
+console.log(obj1)
+
+//Object in JS
+console.log(typeof Object, typeof new Object)
+const obj2 = new Object
+console.log(obj2)
+
+//Constructure Functions
+function Product(name, price, discount){
+    this.name = name
+    this.getPricewithDiscount = () => {
+        return price * (1 - discount)
+    }
+}
+
+const p1 = new Product('Pen', 5.99, 0.15)
+const p2 = new Product('Notebook', 599.99, 0.35)
+console.log(p1.getPricewithDiscount(), p2.getPricewithDiscount())
+
+//Factory Function
+function createEmployee(name, salary, falts) {
+    return{
+        name,
+        salary,
+        falts,
+        getSalary(){
+            return (salary/30) * (30-falts) 
+        }
+    }
+}
+const w1 = createEmployee('John', 700, 1)
+const w2 = createEmployee('Marie', 800, 5)
+console.log(w1.getSalary(), w2.getSalary())
+
+//Obeject.create
+const daughter = Object.create(null)
+daughter.name = 'Emily'
+console.log(daughter)
+
+// Famous Function that returns Object...
+const fromJSON = JSON.parse('{"info": "I am a JSON"}')
+console.log(fromJSON.info)
