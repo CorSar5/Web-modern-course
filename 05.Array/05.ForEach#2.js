c758cddb6f4d5260af27996944c29024d57aa1b3
CorSar <snakexdlol@gmail.com>
2021-01-28 21:56:53 +0000

    NEED TO REVISE 2nd Lesson about Foreach

---
+++ 05.Array/05.ForEach#2.js
@@ -0,0 +1,21 @@
+ function forEach2(array) {
+    for (x = 0; x< array.length; x++){
+    console.log(`${x + 1}) ${array[x]}`) }
+}
+
+const approved = ['Amanda', 'Corney', 'Linda', 'Tatiana']
+
+forEach2(approved)
+
+//
+Array.prototype.forEach2 = function(callback) {
+    for(let i = 0; i< this.length; i++){
+        callback(this[i], i, this)
+    }
+}
+
+const approved = ['Amanda', 'Corney', 'Linda', 'Tatiana']
+
+approved.forEach(function(name,  index) {
+    console.log(`${index +1}) ${name}`)
+});
