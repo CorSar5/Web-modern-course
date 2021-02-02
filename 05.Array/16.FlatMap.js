0e3072119a7ff30b2a150da6b638c0a20aea6d1d
CorSar <snakexdlol@gmail.com>
2021-02-02 01:50:18 +0000

    Flat Map Lesson /Portuguese

---
+++ 05.Array/16.FlatMap.js
@@ -0,0 +1,33 @@
+const escola = [{
+    nome: 'Turma M1',
+    alunos: [{
+        nome: 'Gustavo',
+        nota:8.1
+    },{ nome:'Ana',
+        nota: 9.3
+    }]
+},{
+    nome: 'Turma M2',
+    alunos: [{
+        nome: 'Rebecca',
+        nota: 8.9
+    }, { 
+        nome:'Roberto',
+        nota: 7.3
+    }]
+}]
+
+const getNotaDoAluno = aluno => aluno.nota
+const getNotasdaTurma = turma => turma.alunos.map(getNotaDoAluno)
+
+const notas1 = escola.map(getNotasdaTurma)
+console.log(notas1)
+
+console.log([].concat([ 8.1, 9.3 ], [ 8.9, 7.3 ]))
+
+Array.prototype.flatMap = function (Callback) {
+    return Array.prototype.concat.apply([], this.map(Callback))
+}
+
+const notas2 = escola.flatMap(getNotasdaTurma)
+console.log(notas2)
