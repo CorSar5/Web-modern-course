166ca6761364cad35cf761369dbd79f1dcf349b0
CorSar <snakexdlol@gmail.com>
2021-02-16 19:57:12 +0000

    Passing Parameters

---
+++ 07.Node/09.ParamsPassing.js
@@ -0,0 +1,3 @@
+module.exports = function(...names){
+    return names.map(name => `Good Week ${name}`)
+}
---
+++ 07.Node/09.ParamsPassingClient.js
@@ -0,0 +1,2 @@
+const greetings = require('./09.ParamsPassing')('Anne', 'Mark','John')
+console.log(greetings)
