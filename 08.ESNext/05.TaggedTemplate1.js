29c61a1d8b7cc7f3b43bc3d223dbc9808bae75de
CorSar <snakexdlol@gmail.com>
2021-02-24 11:52:30 +0000

    1st Lesson of Tagged Templates

--- 08.ESNext/05.TaggedTemplate1.js
+++ 08.ESNext/05.TaggedTemplate1.js
@@ -1,10 +1,10 @@
 // Tagged Templates - processes the template in a function
 function tag(parts, ...values){
-    console.log(parts)
-    console.log(values)
+    console.log(parts)// will have indexes like 0, 2, 4
+    console.log(values)// will have indexes like 1, 3, 5
     return 'Another String'
 }
 
 const student = 'Peter'
 const situation = 'Approved'
-console.log(tag `${student} is ${situation}.`)
+console.log(tag `${student} is ${situation}.`)//Will only return 'Another String'
