365f612cc97f13d4f1f4d57e226f2d9aec9688c6
CorSar <snakexdlol@gmail.com>
2021-04-25 18:57:29 +0100

    2nd Lesson of Fundamentals
    Copying from a Folder to another all the .txt documents

--- 14.Gulp/gulp/basico/gulpfile.js
+++ 14.Gulp/gulp/basico/gulpfile.js
@@ -1,9 +1,28 @@
 const gulp = require('gulp')
-const series = gulp.series
+const {series, parallel} = require('gulp')
 
-function copy(cb){
-    console.log('Copy Task')
+const before1 = cb =>{
+    console.log('task before1')
     return cb()
 }
 
-module.exports.default = series(copy)
+const before2 = cb =>{
+    console.log('task before2')
+    return cb()
+}
+
+function copy(cb){
+    //gulp.src(['pastaA/arquivo1.txt','pastaA/arquivo2.txt'])
+    gulp.src('pastaA/**/*.txt')
+        .pipe(gulp.dest('pastaB'))
+    return cb()
+}
+
+const end = cb =>{
+    console.log('task end')
+    return cb()
+}
+
+module.exports.default = series(
+    parallel(before1, before2),
+     copy, end)
