c06dd03cb6e32f3565395033430a29b176768ffb
CorSar <snakexdlol@gmail.com>
2021-04-28 23:26:46 +0100

    cerating the Project Structure

---
+++ 14.Gulp/gulp/spa/gulpTasks/server.js
@@ -0,0 +1,14 @@
+const gulp = require('gulp')
+
+function monitorateArchives(cb){
+    return cb()
+}
+
+function server(cb){
+    return cb()
+}
+
+module.exports = {
+    monitorateArchives,
+    server
+}
