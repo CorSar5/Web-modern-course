c7d6e49ac01560d359f9127deccc5fd8d67d1f6f
CorSar <snakexdlol@gmail.com>
2021-04-29 20:40:33 +0100

    SPA gulp project

--- 14.Gulp/gulp/spa/gulpTasks/app.js
+++ 14.Gulp/gulp/spa/gulpTasks/app.js
@@ -1,24 +1,44 @@
 const gulp = require('gulp')
+const babel  =require('gulp-babel')
+const uglify = require('gulp-uglify')
+const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
+const sass = require('gulp-sass')
+const htmlmin = require('gulp-htmlmin')
+const { logError } = require('gulp-sass')
 
-function appHTML(cb){
 
-    return cb()
+function appHTML(){
+    return gulp.src('src/**/*.html')
+        .pipe(htmlmin({ collapseWhitespace :true}))
+        .pipe(gulp.dest('build'))
 }
 
-function appCSS(cb){
-
-    return cb()
+function appCSS(){
+    return gulp.src('src/assets/sass/index.scss')
+        .pipe(sass().on('error', logError))
+        .pipe(uglifycss({"uglyComments": true}))
+        .pipe(concat('app.min.css'))
+        .pipe(gulp.dest('build/assets/css'))
 }
 
-function appJS(cb){
-
-    return cb()
+function appJS(){
+    return gulp.src('src/assets/js/**/*.js')
+        .pipe(babel({presets: ['ENV']}))
+        .pipe(uglify())
+        .pipe(concat('app.min.js'))
+        .pipe(gulp.dest('build/assets/js'))
 }
 
-function appIMG(cb){
-
-    return cb()
+function appIMG(){
+    return gulp.src('srC/assets/imgs/**/*.*')
+        .pipe(gulp.dest('build/assets/imgs'))
 }
+
+gulp.task('appHTML', appHTML)
+gulp.task('appCSS',appCSS)
+gulp.task('appJS', appJS)
+gulp.task('appIMG', appIMG)
 
 module.exports = {
     appHTML,
--- 14.Gulp/gulp/spa/gulpTasks/server.js
+++ 14.Gulp/gulp/spa/gulpTasks/server.js
@@ -1,11 +1,22 @@
 const gulp = require('gulp')
+const webserver = require('gulp-webserver')
+const watch = require('gulp-watch')
 
 function monitorateArchives(cb){
+    watch('src/**/*.html', () => gulp.series('appHTML')())
+    watch('src/**/*.scss', () => gulp.series('appCSS')())
+    watch('src/**/*.js', () => gulp.series('appJS')())
+    watch('src/assets/imgs/**/*.*', () => gulp.series('appIMG')())
     return cb()
 }
 
 function server(cb){
-    return cb()
+    return gulp.src('build')
+        .pipe(webserver({
+            port: 8083,
+            open: true,
+            livereload: true,
+        }))
 }
 
 module.exports = {
