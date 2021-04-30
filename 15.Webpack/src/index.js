ba43a458c11694225630e2591192ab6618845a78
CorSar <snakexdlol@gmail.com>
2021-04-30 22:18:56 +0100

    1st Example using Webpack
    mode, entry, devDependencies, dist/main.js

---
+++ 15.Webpack/src/index.js
@@ -0,0 +1,5 @@
+//const Person = require('./person')
+import Person from './person'
+
+const clerk = new Person
+console.log(clerk.greetings())
---
+++ 15.Webpack/src/person.js
@@ -0,0 +1,5 @@
+export default class Person {
+    greetings(){
+        return 'Good Moorning!'
+    }
+}
