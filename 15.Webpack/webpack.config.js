bf9603ad2cb01e3d0fe9dc488156799a302dc788
CorSar <snakexdlol@gmail.com>
2021-05-02 22:58:22 +0100

    Extract CSS
    "mini-css-extract-plugin",  new plugin, .loader()

--- 15.Webpack/webpack.config.js
+++ 15.Webpack/webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = { 
     mode : 'development',
@@ -7,11 +8,17 @@ module.exports = {
         filename: 'main.js',
         path: __dirname + '/public'
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "style.css"
+        })
+    ],
     module: {
         rules: [{
             test: /\.css$/,
             use: [
-                'style-loader', //Adds CSS to DOM injecting the tag <style>
+                MiniCssExtractPlugin.loader,
+                //conflict with the upper loarder- 'style-loader', //Adds CSS to DOM injecting the tag <style>
                 'css-loader' //interpreta @import, url()...
             ]
         }]
