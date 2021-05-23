790982baacd68688338fe10baf2f9cce1097760e
CorSar <snakexdlol@gmail.com>
2021-05-23 21:36:41 +0100

    Multiple Components

---
+++ 18.React/exercises/src/components/Mutiple.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const GAfternoon = props => <h1>Good Afternoon {props.name}!</h1>
+
+const GEnvening = props => <h1>Good Evening {props.name}!</h1>
+
+export {GAfternoon, GEnvening}
+export default {GAfternoon, GEnvening}
--- 18.React/exercises/src/index.js
+++ 18.React/exercises/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import GM from './components/GoodMorning'
+import Multi, {GAfternoon} from './components/Mutiple'
 
 const element = <h1>React</h1>
 
 ReactDOM.render(element, document.getElementById('root'))
-ReactDOM.render(<GM name="Mike" />, document.getElementById('root'))
+ReactDOM.render(
+<div>
+    <GAfternoon name="Rachel"/> 
+    <Multi.GEnvening name="John"/>
+    </div>, document.getElementById('root'))
 //Only one will be rendered
