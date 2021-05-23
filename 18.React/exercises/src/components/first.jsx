8ff7ce7f796d376d22de9a18a121eb056f4b50ec
CorSar <snakexdlol@gmail.com>
2021-05-23 16:01:42 +0100

    First Component

---
+++ 18.React/exercises/src/components/first.jsx
@@ -0,0 +1,7 @@
+import React from 'react' 
+//React Import is not needed
+
+export default function(){
+    <h1>First</h1>
+}
+    
--- 18.React/exercises/src/index.js
+++ 18.React/exercises/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import First from './components/first'
+
 const element = <h1>React</h1>
 
 ReactDOM.render(element, document.getElementById('root'))
+ReactDOM.render(<First/>, document.getElementById('root'))//Only one will be rendered
