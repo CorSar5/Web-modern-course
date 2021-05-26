6fc33911d1a0db0abf6c3bc7a535773d7002604a
CorSar <snakexdlol@gmail.com>
2021-05-26 18:56:17 +0100

    Component of the Calculator

--- 18.React/calculator/src/index.js
+++ 18.React/calculator/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Calculator from './main/Calculator'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <h1>Calculator</h1>
+    <Calculator />
+  </div>,
   document.getElementById('root')
 );
 
