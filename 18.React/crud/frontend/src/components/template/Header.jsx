57ec6a1fcb1a31b0c801a1d5c141a9c9a5406914
CorSar <snakexdlol@gmail.com>
2021-05-29 18:59:54 +0100

    Creating The Template Component

---
+++ 18.React/crud/frontend/src/components/template/Header.jsx
@@ -0,0 +1,6 @@
+import './Header.css'
+
+export default props => 
+    <header className="header">
+        Header
+    </header>
---
+++ 18.React/crud/frontend/src/components/template/Main.jsx
@@ -0,0 +1,11 @@
+import './Main.css'
+import Header from './Header'
+import React from 'react'
+
+export default props => 
+    <React.Fragment>
+        <Header/>
+        <main className="content">
+            Content
+        </main>
+    </React.Fragment>
