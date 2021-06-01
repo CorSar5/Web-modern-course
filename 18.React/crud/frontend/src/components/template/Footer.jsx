4fa5dd2ec54e370201b93761719f6349fd25fb10
CorSar <snakexdlol@gmail.com>
2021-06-01 22:18:17 +0100

    Footer styling

--- 18.React/crud/frontend/src/components/template/Footer.jsx
+++ 18.React/crud/frontend/src/components/template/Footer.jsx
@@ -2,5 +2,7 @@ import './Footer.css'
 
 export default props=>
     <footer className="footer">
-        Footer
+        <span>
+            Desenvolved with <i className="fa fa-heart text-danger"></i> by <strong>Cor<span className="text-danger">Sar</span></strong>
+        </span>
     </footer>
