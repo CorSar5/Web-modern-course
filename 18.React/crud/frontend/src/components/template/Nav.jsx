df20e174ed9f89bbe97bf68c4b385ffb6d7be257
CorSar <snakexdlol@gmail.com>
2021-06-02 19:10:12 +0100

    Navigation Style

--- 18.React/crud/frontend/src/components/template/Nav.jsx
+++ 18.React/crud/frontend/src/components/template/Nav.jsx
@@ -2,5 +2,13 @@ import './Nav.css'
 
 export default props=>
     <aside className="menu-area">
-        Navigation
+        <nav className="menu">
+            {/*Refactor, by a new item*/}
+            <a href="#/">
+                <i className="fa fa-home"></i>Home
+            </a>
+            <a href="#/users">
+                <i className="fa fa-users"></i> Users
+            </a>
+        </nav>
     </aside>
