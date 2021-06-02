a814f2df33378417536f59bf1bf3616c0d5a15d5
CorSar <snakexdlol@gmail.com>
2021-06-02 19:22:10 +0100

    User #1

---
+++ 18.React/crud/frontend/src/components/user/UserCrud.jsx
@@ -0,0 +1,18 @@
+import React, { Component } form 'react'
+import Main from '../template/Main'
+
+const headerProps ={
+    icon: 'users',
+    title: 'Users',
+    subtitle:' User Login: Include, List, Change, Exclude'
+}
+
+export default class UserCrud extends Component {
+    render() {
+        return(
+            <Main {...headerProps}>
+                User Login
+            </Main>
+        )
+    }
+}
