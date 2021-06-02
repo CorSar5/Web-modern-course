8a1b7f83bc9e15cb299f220a0f44960badbbc37b
CorSar <snakexdlol@gmail.com>
2021-06-02 19:36:18 +0100

    Implemeting Routes

--- 18.React/crud/frontend/src/components/template/Nav.jsx
+++ 18.React/crud/frontend/src/components/template/Nav.jsx
@@ -1,14 +1,15 @@
 import './Nav.css'
+import { Link } from 'react-router-dom'
 
 export default props=>
     <aside className="menu-area">
         <nav className="menu">
             {/*Refactor, by a new item*/}
-            <a href="#/">
+            <Link to="/">
                 <i className="fa fa-home"></i>Home
-            </a>
-            <a href="#/users">
+            </Link>
+            <Link to="/users">
                 <i className="fa fa-users"></i> Users
-            </a>
+            </Link>
         </nav>
     </aside>
--- 18.React/crud/frontend/src/main/App.jsx
+++ 18.React/crud/frontend/src/main/App.jsx
@@ -1,6 +1,9 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
+import { BrowserRouter } from 'react-router-dom'
+
+import Routes from './Routes'
 
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
@@ -8,9 +11,12 @@ import Home from '../components/home/Home'
 import Footer from '../components/template/Footer'
 
 export default props =>
-    <div className="app">
-        <Logo />
-        <Nav />
-        <Home />
-        <Footer />
-    </div>
+    <BrowserRouter>
+        <div className="app">
+            <Logo />
+            <Nav />
+            <Routes />
+            <Footer />
+        </div>
+    </BrowserRouter>
+    
