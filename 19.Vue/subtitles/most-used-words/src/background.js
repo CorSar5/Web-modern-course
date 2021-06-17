cde06eb69028d504e61323d3ac4949c442ee062c
CorSar <snakexdlol@gmail.com>
2021-06-17 13:42:06 +0100

    Inicial Adjustments

--- 19.Vue/subtitles/most-used-words/src/background.js
+++ 19.Vue/subtitles/most-used-words/src/background.js
@@ -27,14 +27,19 @@ async function createWindow() {
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
     await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
-    if (!process.env.IS_TEST) win.webContents.openDevTools()
+    //if (!process.env.IS_TEST) win.webContents.openDevTools()
   } else {
     createProtocol('app')
     // Load the index.html when not in development
     win.loadURL('app://./index.html')
   }
-}
 
+  const { title, version } = require('../package.json');
+  win.setTitle(`${title} :: ${version}`);
+  win.webContents.on('did-finish-load', () => {
+  })
+
+}
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
