42348d375258c060f4d40c32e9db3e81f428a74d
CorSar <snakexdlol@gmail.com>
2021-06-19 14:55:45 +0100

    Archives Processing #3

--- 19.Vue/subtitles/most-used-words/src/backend/index.js
+++ 19.Vue/subtitles/most-used-words/src/backend/index.js
@@ -2,20 +2,12 @@ const { ipcMain } = require('electron')
 
 const pathsToRows = require('./pathsToRows')
 const prepareData = require('./prepareData')
+const groupWords = require('./groupWords')
 
 ipcMain.on('process-subtitles', (event, paths) => {
 
     pathsToRows(paths)
         .then(rows => prepareData(rows))
-        .then(words => console.log(words))
-        .then(() =>{
-            event.reply('process-subtitles', [
-                { name: 'you', amount: 990 },
-                { name: 'he', amount: 670 },
-                { name: 'i', amount: 630 },
-                { name: 'she', amount: 670 },
-                { name: 'our', amount: 930 },
-                { name: 'Manuela', amount: 9930 },
-            ])
-        })
+        .then(words => groupWords(words))
+        .then(groupedWords => event.reply('process-subtitles', groupedWords))
 })
