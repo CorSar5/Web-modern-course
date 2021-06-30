51c5569a11b20ccfa6fc76fb3407364c70400442
CorSar <snakexdlol@gmail.com>
2021-06-30 18:50:44 +0100

    Obtaining Products

--- 20.Angular/crud/frontend/src/app/app.module.ts
+++ 20.Angular/crud/frontend/src/app/app.module.ts
@@ -25,7 +25,8 @@ import {MatSnackBarModule} from '@angular/material/snack-bar'
 import {HttpClientModule} from '@angular/common/http'
 import {FormsModule} from '@angular/forms'
 import {MatFormFieldModule} from '@angular/material/form-field'
-import {MatInputModule} from '@angular/material/input'
+import {MatInputModule} from '@angular/material/input';
+import { ProductReadComponent } from './components/product/product-read/product-read.component'
 
 
 @NgModule({
@@ -39,7 +40,8 @@ import {MatInputModule} from '@angular/material/input'
     RedDirective,
     ForDirective,
     ProductCreateComponent,
-    ProductCreateComponent
+    ProductCreateComponent,
+    ProductReadComponent
   ],
   imports: [
     BrowserModule,
--- 20.Angular/crud/frontend/src/app/components/product/product-create/product-create.component.ts
+++ 20.Angular/crud/frontend/src/app/components/product/product-create/product-create.component.ts
@@ -18,9 +18,7 @@ export class ProductCreateComponent implements OnInit {
 
   constructor(private productService: ProductService, private router: Router) { }
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
   createProduct(): void{
     this.productService.create(this.product).subscribe(()=> {
