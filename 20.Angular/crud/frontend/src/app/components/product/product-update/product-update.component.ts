d1a2ee171716bb888ffad445263369f48a9b5bf4
CorSar <snakexdlol@gmail.com>
2021-07-04 15:41:38 +0100

    Update Product

--- 20.Angular/crud/frontend/src/app/components/product/product-update/product-update.component.ts
+++ 20.Angular/crud/frontend/src/app/components/product/product-update/product-update.component.ts
@@ -1,3 +1,6 @@
+import { Product } from './../product.model';
+import { ProductService } from './../product.service';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductUpdateComponent implements OnInit {
 
-  constructor() { }
+  product: Product
+
+  constructor(private productService: ProductService, private router: Router, private route: ActivatedRoute){ }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.productService.readById(id).subscribe(product =>{
+      this.product = product;
+    })
   }
 
+  updateProduct(): void{
+    this.productService.update(this.product).subscribe(()=>{
+      this.productService.showMessage('The product was successfully updated!');
+      this.router.navigate(['/products']);
+    });
+  }
+  cancel(): void{
+    this.router.navigate(['/products']);
+  }
 }
