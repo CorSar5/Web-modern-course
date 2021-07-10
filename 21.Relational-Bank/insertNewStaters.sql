0291e6cfd1aad0680c6ade70760a13378b0cfd18
CorSar <snakexdlol@gmail.com>
2021-07-10 14:53:42 +0100

    Inserting new States to be deleted

---
+++ 21.Relational-Bank/insertNewStaters.sql
@@ -0,0 +1,5 @@
+insert into states (id, nome, initials, regions, population1 )
+values  (1000, 'new', 'NV', 'South', 23.4),
+        (1001, 'another', 'AK', 'South', 54.5);
+    
+SELECT * from states
