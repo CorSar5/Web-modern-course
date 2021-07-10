b7b954457efc26c481173b29061766fd5062525f
CorSar <snakexdlol@gmail.com>
2021-07-10 14:08:51 +0100

    Insert States

---
+++ 21.Relational-Bank/insertStates1.sql
@@ -0,0 +1,9 @@
+insert into states (nome, initials, regions, population1)
+VALUES ('Acre', 'AC', 'North', 0.83);
+
+INSERT INTO states
+    (nome, initials, regions, population1)
+Values 
+    ('Alagoas', 'AL', 'North East', 3.38),
+    ('Amapá', 'AP', 'North', 0.8),
+    ('Amazonas', 'AM', 'North', 4.06);
---
+++ 21.Relational-Bank/insertStates2.sql
@@ -0,0 +1,26 @@
+INSERT INTO states
+    (nome, initials, regions, population1)
+VALUES
+    ('Bahia', 'BA', 'North East', 15.34),
+    ('Ceará', 'CE', 'North East', 9.02),
+    ('Distrito Federal', 'DF', 'Midwest', 3.04),
+    ('Espírito Santo', 'ES', 'Southeast', 4.02),
+    ('Goiás', 'GO', 'Midwest', 6.78),
+    ('Maranhao', 'MA', 'North East', 7.00),
+    ('Mato Grosso', 'MT', 'Midwest', 3.34),
+    ('Mato Grosso do South', 'MS', 'Midwest', 2.71),
+    ('Minas Gerais', 'MG', 'Southeast', 21.12),
+    ('Pará', 'PA', 'North', 8.36),
+    ('Paraíba', 'PB', 'North East', 4.03),
+    ('Parana', 'PR', 'South', 11.22),
+    ('Pernambuco', 'PE', 'North East', 9.47),
+    ('Piauí', 'PI', 'North East', 3.22),
+    ('Rio de Janeiro', 'RJ', 'Southeast', 16.72),
+    ('Rio Grande do North', 'RN', 'North East', 3.51),
+    ('Rio Grande do South', 'RS', 'South', 11.32),
+    ('Rondônia', 'RO', 'North', 1.81),
+    ('Roraima', 'RR', 'North', 0.52),
+    ('Santa Catarina', 'SC', 'South', 7.01),
+    ('São Paulo', 'SP', 'Southeast', 45.10),
+    ('Sergipe', 'SE', 'North East', 2.29),
+    ('Tocantins', 'TO', 'North', 1.55);
