9dd8f6a3bb910c19906fbf7c6ba44779f18f3b0d
CorSar <snakexdlol@gmail.com>
2021-07-13 12:38:59 +0100

    Inserting Prefeitos

---
+++ 21.Relational-Bank/insertPref.sql
@@ -0,0 +1,16 @@
+select * from cities
+
+INSERT INTO prefeitos
+    (nome, city_id)
+VALUES
+    ('Rodrigo Neves', 3),
+    ('Raquel Lyra', 4),
+    ('Zenaldo Coutinho', null);
+
+select * from prefeitos
+
+INSERT INTO prefeitos
+    (nome, city_id)
+VALUES
+    ('Rafael Greca', null); --we can repeat NULL values
+
