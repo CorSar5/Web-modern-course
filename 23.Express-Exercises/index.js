b6e791c31d6cd44f62b680a43d9df982b27dc893
CorSar <snakexdlol@gmail.com>
2021-07-19 16:32:59 +0100

    Middleware #1

--- 23.Express-Exercises/index.js
+++ 23.Express-Exercises/index.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const app = express()   
+const salute = require('./saluteMid')
 
-app.get('/sup',(req, res) => {
+app.use(salute('Peter'))
+
+app.use('/sup',(req, res, next) => {
+    console.log('B4')
+    next()
+})
+
+app.get('/sup',(req, res, next) => {
+    console.log('while')
     res.json({
         data: [
             {id:7, name:'Peter', position:1},
@@ -13,21 +22,22 @@ app.get('/sup',(req, res) => {
         limit:3,
         status:200
     })
-    
+    next()
+
     /*res.json([
-        {id:7, name:'Peter', position:1},
-        {id:5, name:'Marie', position:2},
-        {id:2, name:'Steven', position:3}
+        {id:7, name:'Peter', position:1},{id:5, name:'Marie', position:2},{id:2, name:'Steven', position:3}
     ])*/
     
     /*res.json({
-        name: "Mac 1TB",
-        price: 3000.99,
-        discount: 0.05
+        name: "Mac 1TB", price: 3000.99, discount: 0.05
     })*/
 
-
     //res.send("<h1>I'm</h1> <b>fine<b>!")
+})
+
+app.use('/sup',(req, res, next) => {
+    console.log('After')
+    next()
 })
 
 app.listen(3000, ()=>{
