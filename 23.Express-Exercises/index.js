28162eab430ed634f7367605579f83494ad89067
CorSar <snakexdlol@gmail.com>
2021-07-19 13:53:32 +0100

    First Request

--- 23.Express-Exercises/index.js
+++ 23.Express-Exercises/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()   
 
+app.all('/sup',(req, res) => {
+    res.send("I'm <b>fine<b>!")
+})
+
 app.listen(3000, ()=>{
     console.log('Backend Executing ...')
 })
