f8525b83b2e8def7bff90236e13f2a66709a1a9c
CorSar <snakexdlol@gmail.com>
2021-07-20 16:38:41 +0100

    Executing the Final Part of the Project

--- 24.Knowledge/versao-final/backend/knexfile.js
+++ 24.Knowledge/versao-final/backend/knexfile.js
@@ -1,6 +1,12 @@
 const { db } = require('./.env')
 
 module.exports = {
+    client: 'postgresql',
+    connection: {
+        database: 'knowledge_final',
+        user: 'postgres',
+        password: 'abcd12' // sua senha 
+},
 	client: 'postgresql',
 	connection: db,
 	pool: {
@@ -11,3 +17,4 @@ module.exports = {
 		tableName: 'knex_migrations'
 	}
 };
+
