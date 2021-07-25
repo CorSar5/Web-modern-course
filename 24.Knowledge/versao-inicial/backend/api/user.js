9db23743574b66fdd8685a10e1827a312a96308c
CorSar <snakexdlol@gmail.com>
2021-07-25 11:31:50 +0100

    admin Validation

--- 24.Knowledge/versao-inicial/backend/api/user.js
+++ 24.Knowledge/versao-inicial/backend/api/user.js
@@ -13,6 +13,9 @@ module.exports = app => {
         const user = {...req.body}
         if (req.params.id) user.id = req.params.id
 
+        if(!req.originalUrl.startsWith('/users')) user.admin = false
+        if(!req.user || !req.user.admin) user.admin = false
+
         try {
             existsOrError(user.name, 'Name not informed')
             existsOrError(user.email, 'E-mail not informed')
