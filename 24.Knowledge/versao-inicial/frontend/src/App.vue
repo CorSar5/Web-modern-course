93731ff445906fc809378af590723de8035b08db
CorSar <snakexdlol@gmail.com>
2021-07-27 02:45:26 +0100

    Menu Visibility

--- 24.Knowledge/versao-inicial/frontend/src/App.vue
+++ 24.Knowledge/versao-inicial/frontend/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-	<div id="app">
+	<div id="app" :class="{'hide-menu': !isMenuVisible}">
 		<Header title="Cod3r - Knowledge Base" :hideToggle="false"/>
 		<Menu/>	
 		<Content/>
@@ -8,6 +8,7 @@
 </template>
 
 <script>
+import {mapState} from 'vuex'
 import Header from "@/components/template/Header.vue"
 import Menu from "@/components/template/Menu.vue"
 import Content from "@/components/template/Content.vue"
@@ -15,7 +16,9 @@ import Footer from "@/components/template/Footer.vue"
 
 export default {
 	name: "App",
-	components: {Header, Menu, Content, Footer}
+	components: {Header, Menu, Content, Footer},
+	computed: mapState(['isMenuVisible'])
+	
 }
 </script>
 	
@@ -41,5 +44,9 @@ export default {
 			"menu content"
 			"menu footer"
 			;
+	}
+
+	#app.hide-menu{
+		grid-template-areas:"header header" "content content" "footer footer";
 	}
 </style>
