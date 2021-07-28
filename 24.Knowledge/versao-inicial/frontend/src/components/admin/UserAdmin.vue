18cca4363709036248ef8069a43b41d5d5e33a2f
CorSar <snakexdlol@gmail.com>
2021-07-28 02:18:44 +0100

    User Log In #1

--- 24.Knowledge/versao-inicial/frontend/src/components/admin/UserAdmin.vue
+++ 24.Knowledge/versao-inicial/frontend/src/components/admin/UserAdmin.vue
@@ -1,12 +1,41 @@
 <template>
   <div class="user-admin">
-      <h1>Component User</h1>
+      <b-table hover striped :items="users" :fields=fields></b-table>
   </div>
 </template>
 
 <script>
+import { baseApiUrl } from '@/global'
+import axios from 'axios'
+
 export default {
-    name: 'UserAdmin'
+    name: 'UserAdmin',
+    data: function(){
+        return{
+            mode: 'save',
+            user: {},
+            users: [],
+            fields: [
+                {key: 'id', label: 'Code', sortable: true},
+                {key: 'name', label: 'Name', sortable: true},
+                {key: 'email', label: 'E-mail', sortable: true},
+                {key: 'admin', label: 'Administrator', sortable: true, 
+                formatter: value => value ? 'Yes': 'No'},
+                {key: 'actions', label: 'Actions'}
+            ]
+        }
+    },
+    methods: {
+        loadUsers() {
+            const url = `${baseApiUrl}/users`
+            axios.get(url).then( res => {
+                this.users = res.data
+            })
+        }
+    },
+    mounted(){
+        this.loadUsers()
+    }
 }
 </script>
 
