1a5a76b88bdd6f6ae4cf543074a70c3974d92c50
CorSar <snakexdlol@gmail.com>
2021-07-28 01:54:29 +0100

    Home Component

--- 24.Knowledge/versao-inicial/frontend/src/config/store.js
+++ 24.Knowledge/versao-inicial/frontend/src/config/store.js
@@ -19,7 +19,6 @@ export default new Vuex.Store({
                 state.isMenuVisible = isVisible
             }
 
-            console.log('toggleMenu = '+state.isMenuVisible)
         }
     }
 })
