9164365405e7d2fad61fc009293a02c00ad5ec36
CorSar <snakexdlol@gmail.com>
2021-07-28 02:01:55 +0100

    Admin Pages

--- 24.Knowledge/versao-inicial/frontend/src/components/admin/AdminPages.vue
+++ 24.Knowledge/versao-inicial/frontend/src/components/admin/AdminPages.vue
@@ -2,15 +2,33 @@
     <div class="admin-pages">
         <PageTitle icon="fa fa-cogs" main="System Administration"
          sub="Logs & Companie"/>
+         <div class="admin-pages-tabs">
+             <b-card no-body>
+                 <b-tabs card>
+                     <b-tab title="Articles" active>
+                         <ArticleAdmin />
+                     </b-tab>
+                     <b-tab title="Categories">
+                         <CategoryAdmin/>
+                     </b-tab>
+                     <b-tab title="Users">
+                         <UserAdmin/>
+                     </b-tab>
+                 </b-tabs>
+             </b-card>
+         </div>
     </div>
 </template>
 
 <script>
 import PageTitle from '../template/PageTitle.vue'
+import ArticleAdmin from './ArticleAdmin'
+import CategoryAdmin from './CategoryAdmin'
+import UserAdmin from './UserAdmin'
 
 export default {
     name: 'AdminPages',
-    components: { PageTitle }
+    components: { PageTitle, ArticleAdmin, CategoryAdmin, UserAdmin }
 }
 </script>
 
