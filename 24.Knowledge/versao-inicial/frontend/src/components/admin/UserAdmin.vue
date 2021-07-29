5af6ba2cb264bf169baf3850cb8bc4cd6d3d6e7f
CorSar <snakexdlol@gmail.com>
2021-07-29 01:49:32 +0100

    User Log in #2

--- 24.Knowledge/versao-inicial/frontend/src/components/admin/UserAdmin.vue
+++ 24.Knowledge/versao-inicial/frontend/src/components/admin/UserAdmin.vue
@@ -1,11 +1,54 @@
 <template>
   <div class="user-admin">
+      <b-form>
+          <input id="user-id" type="hidden" v-model="user.id">
+          <b-row>
+              <b-col md="6" sm="12">
+                  <b-form-group label="Name:" label-for="user-name">
+                      <b-form-input id="user-name" type="text"
+                      v-model="user.name" required 
+                      placeholder="Inform the Username"/>
+                  </b-form-group>
+              </b-col>
+              <b-col md="6" sm="12">
+                  <b-form-group label="E-mail:" label-for="user-email">
+                      <b-form-input id="user-email" type="text"
+                      v-model="user.email" required 
+                      placeholder="Inform the user's E-mail"/>
+                  </b-form-group>
+              </b-col>
+          </b-row>
+          <b-form-checkbox id="user-admin" v-model="user.admin" 
+          class="mt-3 mb-3"> Admin?</b-form-checkbox>
+          <b-row>
+              <b-col md="6" sm="12">
+                  <b-form-group label="Password:" label-for="user-password">
+                      <b-form-input id="user-password" type="password"
+                      v-model="user.password" required 
+                      placeholder="Inform the Password"/>
+                  </b-form-group>
+              </b-col>
+              <b-col md="6" sm="12">
+                  <b-form-group label="Password Confirmation:" label-for="user-confirm-password">
+                      <b-form-input id="user-confirm-password" type="password"
+                      v-model="user.confirmPassword" required 
+                      placeholder="Confirm the user's password"/>
+                  </b-form-group>
+              </b-col>
+          </b-row>
+          <b-button variant="primary" v-if="mode === 'save'"
+          @click="save">Save</b-button>
+          <b-button variant="danger" v-if="mode === 'remove'"
+          @click="remove">Delete</b-button>
+          <b-button class="ml-2" @click="reset">Cancel</b-button>
+      </b-form>
+      <hr>
       <b-table hover striped :items="users" :fields=fields></b-table>
   </div>
 </template>
 
 <script>
-import { baseApiUrl } from '@/global'
+import { baseApiUrl, showError } from '@/global'
 import axios from 'axios'
 
 export default {
@@ -31,6 +74,30 @@ export default {
             axios.get(url).then( res => {
                 this.users = res.data
             })
+        },
+        reset() {
+            this.mode = 'save'
+            this.user= {}
+            this.loadUsers()
+        },
+        save() {
+            const method = this.user.id ? 'put' : 'post'
+            const id = this.user.id ? `/${this.user.id}` : ''
+            axios[method](`${baseApiUrl}/users${id}`, this.user)
+                .then(() => {
+                    this.$toasted.global.defaultSuccess()
+                    this.reset()
+                })
+                .catch(showError)
+        }, 
+        remove(){
+            const id = this.user.id
+            axios.delete(`${baseApiUrl}/users/${id}`)
+                .then(()=>{
+                    this.$toasted.global.defaultSuccess()
+                    this.reset()
+            })
+            .catch(showError)
         }
     },
     mounted(){
