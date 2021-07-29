62484f335755d276609b9a532261fd9c642811f9
CorSar <snakexdlol@gmail.com>
2021-07-29 19:41:57 +0100

    Articles By Category #2

--- 24.Knowledge/versao-inicial/frontend/src/components/article/ArticlesByCategory.vue
+++ 24.Knowledge/versao-inicial/frontend/src/components/article/ArticlesByCategory.vue
@@ -2,6 +2,15 @@
     <div class="articles-by-category">
         <PageTitle icon="fa fa-folder-o" :main="category.name"
         sub="Category" />
+        <ul>
+            <li v-for="article in articles" :key="article.id" >
+                {{ article.name }}
+            </li>
+            <div class="load-more">
+                <button v-if="loadMore" 
+                class="btn btn-lg btn-outline-primary" @click="getArticles">Load more Articles</button>
+            </div>
+        </ul>
     </div>
 </template>
 
@@ -25,15 +34,35 @@ export default {
         getCategory() {
             const url = `${baseApiUrl}/categories/${this.category.id}`
             axios(url).then( res => this.category = res.data)
+        },
+        getArticles(){
+            const url= `${baseApiUrl}/categories/${this.category.id}/articles?page=${this.page}`
+            axios(url).then(res => {
+                this.articles = this.articles.concat(res.data)
+                this.page++
+
+                if(res.data.length === 0) this.loadMore = false
+            })
         }
     },
     mounted(){
         this.category.id = this.$route.params.id
         this.getCategory()
+        this.getArticles()
     }
 }
 </script>
 
 <style>
+    .articles-by-category ul{
+        padding: 0;
+        list-style-type: none;
+    }
 
+    .articles-by-category .load-more{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-top: 25px;
+    }
 </style>
