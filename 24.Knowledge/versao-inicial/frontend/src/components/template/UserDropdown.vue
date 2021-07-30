156b9cd74e42bee3cf9b27ed1eda502aab73721b
CorSar <snakexdlol@gmail.com>
2021-07-30 19:24:05 +0100

    Authentication: Logout

--- 24.Knowledge/versao-inicial/frontend/src/components/template/UserDropdown.vue
+++ 24.Knowledge/versao-inicial/frontend/src/components/template/UserDropdown.vue
@@ -9,19 +9,27 @@
         </div>
         <div class="user-dropdown-content">
             <router-link to="/admin"><i class="fa fa-cogs"></i> Administration</router-link>
-            <router-link to="/admin"><i class="fa fa-sign-out"> Sign Out</i></router-link>
+            <a href @click.prevent="logout"><i class="fa fa-sign-out"> Sign Out</i></a>
         </div>
     </div>
 </template>
 
 <script>
+import { userKey } from '@/global'
 import {mapState} from 'vuex'
 import Gravatar from 'vue-gravatar'
 
 export default {
     name: 'UserDropdown',
     components: {Gravatar},
-    computed: mapState(['user'])
+    computed: mapState(['user']),
+    methods: {
+        logout(){
+            localStorage.removeItem(userKey)
+            this.$store.commit('setUser', null)
+            this.$router.push({name: 'auth'})
+        }
+    }
 }
 </script>
 
