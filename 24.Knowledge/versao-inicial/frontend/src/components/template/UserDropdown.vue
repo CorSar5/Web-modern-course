39fd2480c67972c63eada055e6f72ef14f78ae36
CorSar <snakexdlol@gmail.com>
2021-07-30 19:47:23 +0100

    Authentication: Validating Admin

--- 24.Knowledge/versao-inicial/frontend/src/components/template/UserDropdown.vue
+++ 24.Knowledge/versao-inicial/frontend/src/components/template/UserDropdown.vue
@@ -8,7 +8,7 @@
             <i class="fa fa-angle-down"></i>
         </div>
         <div class="user-dropdown-content">
-            <router-link to="/admin"><i class="fa fa-cogs"></i> Administration</router-link>
+            <router-link to="/admin" v-if="user.admin"><i class="fa fa-cogs"></i> Administration</router-link>
             <a href @click.prevent="logout"><i class="fa fa-sign-out"> Sign Out</i></a>
         </div>
     </div>
